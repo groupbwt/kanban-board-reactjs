7b05fd27fcfa5eec4745fd2840d21c6d12e98f0b
pastushenko_sv <pastushenko_sv@groupbwt.com>
2020-06-24 18:33:35 +0300

    add toggle add a card

--- src/components/Columns/Column/Column.jsx
+++ src/components/Columns/Column/Column.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Button } from 'components/Button/Button';
 import { ColumnTitle } from '../ColumnTitle/ColumnTitle';
 import { ColumnCards } from '../ColumnCards/ColumnCards';
 import styles from './Column.module.scss';
-import { Textarea } from "../../Textarea/Textarea";
+import { Textarea } from '../../Textarea/Textarea';
 
 Column.propTypes = {
   title: PropTypes.string,
-  cards: PropTypes.arrayOf(),
+  cards: PropTypes.arrayOf(PropTypes.object),
   className: PropTypes.string,
 };
 Column.defaultProps = {
@@ -19,6 +19,12 @@ Column.defaultProps = {
 };
 
 function Column({ title, cards, className }) {
+  const [isStartedCreatingCard, setIsStartedCreatingCard] = useState(false);
+
+  function toggleStartCreatCard() {
+    setIsStartedCreatingCard((prevState) => !prevState);
+  }
+
   return (
     <div className={`${styles.column} ${className}`}>
       {title && (
@@ -30,19 +36,44 @@ function Column({ title, cards, className }) {
       <div className={styles.column__cards}>
         <ColumnCards cards={cards} />
 
-        <Textarea
-          className={styles.column__textarea}
-          placeholder='Enter a title for this card...'
-          minRows={3}
-          maxRows={7}
-        />
+        {isStartedCreatingCard && (
+          <Textarea
+            className={styles.column__textarea}
+            placeholder="Enter a title for this card..."
+            minRows={3}
+            maxRows={7}
+          />
+        )}
       </div>
-      <Button
-        className={styles.column__btn}
-        icon={<FontAwesomeIcon icon={faPlus} size="lg" />}
-      >
-        Add a card
-      </Button>
+      <div className={styles.column__buttons}>
+        {isStartedCreatingCard ? (
+          <>
+            <Button
+              onClick={toggleStartCreatCard}
+              className={styles['column__btn--create']}
+              color="green"
+              icon={<FontAwesomeIcon icon={faPlus} size="md" />}
+            >
+              Add Card
+            </Button>
+            <Button
+              displayType="icon"
+              onClick={toggleStartCreatCard}
+              className={styles['column__btn--cancel']}
+              color="transparent"
+              icon={<FontAwesomeIcon icon={faTimes} size="lg" />}
+            />
+          </>
+        ) : (
+          <Button
+            onClick={toggleStartCreatCard}
+            className={styles.column__btn}
+            icon={<FontAwesomeIcon icon={faPlus} size="md" />}
+          >
+            Add a card
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
