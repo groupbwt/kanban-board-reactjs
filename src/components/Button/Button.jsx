c9492a896c320d6658e944c5ca3588d21188e5bc
pastushenko_sv <pastushenko_sv@groupbwt.com>
2020-06-24 18:30:59 +0300

    add button type icon for Button component

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,22 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import styles from './Button.module.scss';
 
 Button.propTypes = {
+  type: PropTypes.string,
+  displayType: PropTypes.string,
   children: PropTypes.node,
   icon: PropTypes.node,
   className: PropTypes.string,
+  color: PropTypes.string,
 };
 
-function Button({ children, icon, className, ...restProps }) {
+Button.defaultProps = {
+  displayType: 'default',
+  type: 'button',
+};
+
+function Button({
+  type,
+  displayType,
+  children,
+  icon,
+  className,
+  color,
+  ...restProps
+}) {
   return (
     <button
-      type="button"
-      className={`${styles.button} ${className}`}
+      type={type}
+      className={classnames({
+        [styles.button]: true,
+        [className]: !!className,
+        [styles.icon]: displayType === 'icon',
+        [styles.green]: color === 'green',
+        [styles.transparent]: color === 'transparent',
+      })}
       {...restProps}
     >
       {icon && <span>{icon}</span>}
-      <span className={styles.button__text}>{children}</span>
+      {displayType !== 'icon' && (
+        <span className={styles.button__text}>{children}</span>
+      )}
     </button>
   );
 }
