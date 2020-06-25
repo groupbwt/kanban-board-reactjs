f64f9797c5958056c6f89fc48d1be9b0c34e176a
pastushenko_sv <pastushenko_sv@groupbwt.com>
2020-06-25 14:08:23 +0300

    add connect Dashboard with store

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Dashboard } from 'views/Dashboard/Dashboard';
+import Dashboard from 'views/Dashboard/Dashboard';
 
 function App() {
   return <Dashboard />;
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,42 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { injectSaga } from 'redux-injectors';
 import { Columns } from 'components/Columns/Columns';
+import { getTasks } from 'redux/tasks/sagas';
+import { TasksSelectors } from 'redux/tasks';
 import styles from './Dashboard.module.scss';
 
 class Dashboard extends Component {
   render() {
+    const { tasks } = this.props;
     return (
       <div className={styles.dashboard}>
-        <Columns
-          columns={[
-            {
-              id: 1,
-              title: 'Plan on month',
-              cards: [
-                { id: 1, title: 'Test' },
-                { id: 2, title: 'Test' },
-              ],
-            },
-            {
-              id: 2,
-              title: 'Plan on day',
-              cards: [
-                { id: 1, title: 'Test' },
-                { id: 2, title: 'Test' },
-              ],
-            },
-            {
-              id: 3,
-              title: 'Plan on month',
-              cards: [
-                { id: 1, title: 'Test' },
-                { id: 2, title: 'Test' },
-              ],
-            },
-          ]}
-        />
+        <Columns columns={tasks} />
       </div>
     );
   }
 }
 
-export { Dashboard };
+Dashboard.propTypes = {
+  tasks: PropTypes.arrayOf(PropTypes.object),
+};
+
+Dashboard.defaultProps = {
+  tasks: [],
+};
+
+const mapStateToProps = (state) => ({
+  tasks: TasksSelectors.selectAll(state),
+});
+
+export default connect(mapStateToProps)(
+  injectSaga({ key: 'books', saga: getTasks })(Dashboard)
+);
