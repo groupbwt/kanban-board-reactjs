cd62e91d33faf75b29e4f64d03c432628ad1e40e
pastushenko_sv <pastushenko_sv@groupbwt.com>
2020-06-26 16:09:02 +0300

    Updates

---
+++ precache-manifest.9615e2138befa9f4d9ea61b2faf9a7b5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d258836652b67d3fd11bb9215c805f0d",
+    "url": "/kanban-board-reactjs/index.html"
+  },
+  {
+    "revision": "6419c5dbb64bebaf385c",
+    "url": "/kanban-board-reactjs/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b3249a136050a155fba0",
+    "url": "/kanban-board-reactjs/static/css/main.6c1c234a.chunk.css"
+  },
+  {
+    "revision": "6419c5dbb64bebaf385c",
+    "url": "/kanban-board-reactjs/static/js/2.7e3a650e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kanban-board-reactjs/static/js/2.7e3a650e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3249a136050a155fba0",
+    "url": "/kanban-board-reactjs/static/js/main.229c5867.chunk.js"
+  },
+  {
+    "revision": "f6a95dabc91673593f07",
+    "url": "/kanban-board-reactjs/static/js/runtime-main.48a6f2d8.js"
+  },
+  {
+    "revision": "d2ddeae5dc92ad95f4f31b3030f75afd",
+    "url": "/kanban-board-reactjs/static/media/dashboard-bg.d2ddeae5.jpg"
+  }
+]);
