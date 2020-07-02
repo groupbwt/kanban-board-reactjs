654353c125af838a42b601f805346052db93503a
pastushenko_sv <pastushenko_sv@groupbwt.com>
2020-07-02 14:26:23 +0300

    add closing modal by clicking on overlay

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,19 +6,25 @@ import styles from './Modal.module.scss';
 Modal.propTypes = {
   maxWidth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
   children: PropTypes.node,
+  onClickOverlay: PropTypes.func,
 };
 
 Modal.defaultProps = {
-  maxWidth: 350
+  maxWidth: 350,
+  onClickOverlay: () => {},
 };
 
-function Modal({ maxWidth, children }) {
+function Modal({ maxWidth, children, onClickOverlay }) {
   const modal = (
-    <div className={styles.modal}>
-      <div className={styles.modal__content} style={{ maxWidth }}>
+    <div className={styles.modal} role="presentation">
+      <div className={styles.modal__content} role="dialog" style={{ maxWidth }}>
         {children}
       </div>
-      <div className={styles.modal__overlay} />
+      <div
+        className={styles.modal__overlay}
+        aria-hidden="true"
+        onClick={onClickOverlay}
+      />
     </div>
   );
 
