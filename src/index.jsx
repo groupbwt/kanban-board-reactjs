d21bbab58a5634e556aa2aacf8630d0b6a38c4d8
pastushenko_sv <pastushenko_sv@groupbwt.com>
2020-07-03 14:01:07 +0300

    add polyfills for IE11

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import 'normalize.css';
